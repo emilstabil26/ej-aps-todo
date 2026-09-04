17874033b3daf1f909b394250f99fe7e1e97d142
emilstabil26 <7j645b6g2k@privaterelay.appleid.com>
2026-09-04 04:01:42 +0200

    Opdater app-cache til det enkle layout

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='ejaps-v5';
-const ASSETS=['./','./index.html','./styles.css','./app-v5.js','./manifest.webmanifest','./icon.svg'];
+const CACHE='ejaps-v6';
+const ASSETS=['./','./index.html','./styles.css','./simple.css','./app-v5.js','./simple-ui.js','./manifest.webmanifest','./icon.svg'];
 self.addEventListener('install',event=>{self.skipWaiting();event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)))});
 self.addEventListener('activate',event=>{event.waitUntil(Promise.all([caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))),self.clients.claim()]))});
 self.addEventListener('fetch',event=>{if(event.request.method!=='GET')return;event.respondWith(fetch(event.request).then(response=>{const copy=response.clone();caches.open(CACHE).then(cache=>cache.put(event.request,copy));return response}).catch(()=>caches.match(event.request).then(response=>response||caches.match('./index.html'))))});
