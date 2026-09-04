5276c00aeb935bb4a0c62047ff4d736798ee183f
emilstabil26 <7j645b6g2k@privaterelay.appleid.com>
2026-09-04 14:07:27 +0200

    Cache Activity and Firma enhancements

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='ejaps-v13';
-const ASSETS=['./','./index.html','./styles.css','./simple.css','./brand.css','./task-help.css','./focus.css','./company-chat.css','./app-v5.js','./name-picker.js','./simple-ui.js','./task-help.js','./focus.js','./company-chat.js','./manifest.webmanifest','./icon-192.png','./icon-512.webp'];
+const CACHE='ejaps-v14';
+const ASSETS=['./','./index.html','./styles.css','./simple.css','./brand.css','./task-help.css','./focus.css','./company-chat.css','./app-v5.js','./name-picker.js','./simple-ui.js','./task-help.js','./focus.js','./company-chat.js','./company-enhancements.js','./manifest.webmanifest','./icon-192.png','./icon-512.webp'];
 self.addEventListener('install',event=>{self.skipWaiting();event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)))});
 self.addEventListener('activate',event=>{event.waitUntil(Promise.all([caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))),self.clients.claim()]))});
 self.addEventListener('fetch',event=>{if(event.request.method!=='GET')return;const url=new URL(event.request.url);if(url.origin!==self.location.origin)return;event.respondWith(fetch(event.request).then(response=>{const copy=response.clone();caches.open(CACHE).then(cache=>cache.put(event.request,copy));return response}).catch(()=>caches.match(event.request).then(response=>response||caches.match('./index.html'))))});
